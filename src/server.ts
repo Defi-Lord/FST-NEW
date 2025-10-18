a7f16d1c227d4d7449541ea561e6ee2fd93e93b5
Defi-Lord <dammypeck@gmail.com>
2025-10-18 17:35:42 +0100

    Initial working backend scaffold

--- src/server.ts
+++ src/server.ts
@@ -19,3 +19,6 @@ app.use('/api', routes);
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("🏆 Welcome to the FST Fantasy Football API!");
+});
